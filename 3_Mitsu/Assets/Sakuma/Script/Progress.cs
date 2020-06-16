5010d012e1ff4278739b37633214dce284017327
sakuma <syoma.0415@gmail.com>
2020-06-16 16:14:43 +0900

    [New] シーン作成

---
+++ 3_Mitsu/Assets/Sakuma/Script/Progress.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+///ゲームの進行度を管理するクラス
+/// </summary>
+public class Progress : MonoBehaviour
+{
+
+    //Instance
+    static public Progress Instance;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}
