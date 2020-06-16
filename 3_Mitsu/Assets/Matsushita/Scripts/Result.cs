9ec1d42683ad52ce7b71c5915996642d8676498f
松下 <matsuniisan@gmail.com>
2020-06-16 16:29:36 +0900

    タイトルとリザルトシーンの追加とシーン遷移

---
+++ 3_Mitsu/Assets/Matsushita/Scripts/Result.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Result : MonoBehaviour
+{
+    private Button resultButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        resultButton = GetComponent<Button>();
+
+        resultButton.onClick.AddListener(() =>
+        {
+            //シーンの名前を変えよう。
+            //SceneManager.LoadScene("TitleScene");
+
+            Debug.Log("リザルトのボタンが押された");
+        });
+    }
+
+
+}
---
+++ 3_Mitsu/Assets/Matsushita/Scripts/StartButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartButton : MonoBehaviour
+{
+    private Button Startbutton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Startbutton = GetComponent<Button>();
+
+        Startbutton.onClick.AddListener(() =>
+        {
+            //シーンの名前を変えよう。
+            //SceneManager.LoadScene("MainScene");
+
+            Debug.Log("ボタンが押された");
+        });
+    }
+
+
+}
