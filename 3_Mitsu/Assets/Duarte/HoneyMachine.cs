9cda7b23fbb522ec2ccde886aad05454684770d9
DavidDuarte1993 <49542308+DavidDuarte1993@users.noreply.github.com>
2020-07-05 09:02:43 +0900

    Add files via upload

---
+++ 3_Mitsu/Assets/Duarte/HoneyMachine.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoneyMachine : MonoBehaviour
+{
+    //instances
+    public Player player;
+    public Storehouse store;
+    public static HoneyMachine instance;
+
+    [SerializeField]
+    public float time;
+
+    public int honeyinmachine;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+        store = FindObjectOfType<Storehouse>();
+    }
+
+    void Update()
+    {
+    }
+
+    //蜂蜜製造機に近づいてスペースキーで持っている蜂国をすべて製造機に入れる(Collider)
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            if (Input.GetKeyDown(KeyCode.Space)) 
+            {
+                MakeHoney();
+            }
+        }
+    }
+
+
+    public void MakeHoney()
+    {
+        //製造機は時間をかけて蜂蜜を生成
+        player.takenhoney = honeyinmachine;
+        player.takenhoney = 0;
+        time -= Time.deltaTime;
+        if(time == 0)
+        {
+            //完成した蜂蜜は保管庫に追加する
+            store.honeyinstore += honeyinmachine;
+            honeyinmachine = 0;
+        }
+    }
+}
