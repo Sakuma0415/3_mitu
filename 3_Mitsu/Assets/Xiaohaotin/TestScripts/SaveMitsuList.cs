52b820358ec26bb47b3ae3b1f7b82181b95f8a80
xht <xht19970808@gmail.com>
2020-07-07 17:41:07 +0900

    保管庫を追加しました。

---
+++ 3_Mitsu/Assets/Xiaohaotin/TestScripts/SaveMitsuList.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveMitsuList : MonoBehaviour
+{
+    public GameObject itemTemplate;
+
+    public GameObject content;
+
+    public void AddButton_Click()
+    {
+        var copy = Instantiate(itemTemplate);
+        copy.transform.parent = content.transform;
+    }
+}
