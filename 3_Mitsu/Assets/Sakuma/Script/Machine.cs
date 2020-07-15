1c8a4ddb7d9648d3b3faf8e6400b55bcdc4f4a82
sakuma <syoma.0415@gmail.com>
2020-07-15 18:18:19 +0900

    製造機

---
+++ 3_Mitsu/Assets/Sakuma/Script/Machine.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Machine : MonoBehaviour
+{
+
+    int line = 0;
+    float[] lineTime = new float[1];
+    bool[] IsLine = new bool[1];
+    public int stack = 0;
+    public int end = 0;
+    [SerializeField]
+    GameObject[] GageObj;
+    [SerializeField]
+    Image[] images;
+    [SerializeField]
+    Text contText;
+    [SerializeField]
+    float siftTime =10;
+
+    void Start()
+    {
+        line = 1;
+        lineTime[0] = 0;
+        for(int i=0;i< GageObj.Length; i++)
+        {
+
+            GageObj[i].SetActive(i<line);
+        }
+    }
+    
+    void Update()
+    {
+
+        contText.text  = stack.ToString();
+        
+        for (int j = 0; j < IsLine.Length; j++)
+        {
+            if (!IsLine[j]&&stack >0)
+            {
+                IsLine[j] = true;
+                stack--;
+            }
+        }
+
+
+        if (Input .GetKeyDown(KeyCode.Q))
+        {
+            LinePlus();
+        }
+
+        if (GameStatus.Instance.gameMode == GameStatus .GameMode .Play )
+        {
+            sysGo();
+        }
+    }
+
+    void sysGo()
+    {
+        for (int j = 0; j < IsLine.Length; j++)
+        {
+            if (IsLine[j])
+            {
+                lineTime[j] += Time.deltaTime;
+            }
+
+            if(lineTime[j]> siftTime)
+            {
+                lineTime[j] = 0;
+                IsLine[j] = false;
+                end += 1;
+            }
+            images[j].fillAmount = lineTime[j] / siftTime;
+        }
+    }
+
+
+
+    public void LinePlus()
+    {
+        if (line < 3)
+        {
+            line += 1;
+
+            Array.Resize(ref lineTime, lineTime.Length + 1);
+            lineTime[lineTime.Length - 1] = 0;
+
+            Array.Resize(ref IsLine, IsLine.Length + 1);
+            IsLine[IsLine.Length - 1] = false;
+
+            for (int i = 0; i < GageObj.Length; i++)
+            {
+                GageObj[i].SetActive(i < line);
+            }
+        }
+    }
+
+    public void StackH()
+    {
+        for(int i=0;i<ItemList.Instance .itemList.Length; i++)
+        {
+            if (ItemList.Instance.itemList[i]!= -1)
+            {
+                if (ItemList.Instance.itemDataList[ItemList.Instance.itemList[i]].itemName == "ハチの巣")
+                {
+                    stack++;
+                    ItemList.Instance.itemList[i] = -1;
+                }
+            }
+        }
+    }
+
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (LayerMask.LayerToName(collision.gameObject.layer) == "Player")
+        {
+            if(Input .GetKeyDown (KeyCode.Space))
+            {
+                StackH();
+            }
+        }
+    }
+
+}
---
+++ 3_Mitsu/Assets/Sakuma/Script/Storage.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Storage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        texts.text = machine.end.ToString();
+    }
+
+    [SerializeField]
+    Machine machine;
+    [SerializeField]
+    Text texts;
+
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (LayerMask.LayerToName(collision.gameObject.layer) == "Player")
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                
+                ItemList.Instance.okane += machine.end * 1000;
+                machine.end = 0;
+
+            }
+        }
+    }
+}
