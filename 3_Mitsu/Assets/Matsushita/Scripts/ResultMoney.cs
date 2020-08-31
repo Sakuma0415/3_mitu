62a5285ce182c514cc5b386920e96f8ea7e9b1da
松下 <matsuniisan@gmail.com>
2020-08-31 13:53:01 +0900

    リザルトに稼いだお金を表示するようにしました。

---
+++ 3_Mitsu/Assets/Matsushita/Scripts/ResultMoney.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResultMoney : MonoBehaviour
+{
+    private Text earnMoneyText;
+    private int firstscore = 0;
+    private int gameScore = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        earnMoneyText.text = firstscore.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddScore()
+    {
+        if(firstscore<gameScore)
+        {
+            firstscore += 1;
+            earnMoneyText.text = firstscore.ToString();
+        }
+    }
+}
